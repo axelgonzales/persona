bb80e788e2e415781d9ae38624379467f65e2732
Axel <ac.gonzalesllerena@gmail.com>
2020-07-27 14:53:49 -0500

    agregar context-path

--- src/main/java/pe/inkacola/rrhh/repository/PersonaRepository.java
+++ src/main/java/pe/inkacola/rrhh/repository/PersonaRepository.java
@@ -1,11 +1,10 @@
 package pe.inkacola.rrhh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import pe.inkacola.rrhh.domain.Persona;
 
-@Repository
+
 public interface PersonaRepository extends JpaRepository<Persona,Integer> {
 
 }
